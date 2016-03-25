5449d1ebfa5247874e2392af242bd477cbdac1fc
EnzoCano <enzoo.canoo@gmail.com>
2016-03-25 12:07:23 +0100

    Add turn function
    Turn right when detect(true)

--- Autonomie/Mission_1/Main/Main.ino
+++ Autonomie/Mission_1/Main/Main.ino
@@ -3,25 +3,38 @@
 
 #define LEFT_SERVO_PIN 15
 #define RIGHT_SERVO_PIN 14
+#define SENSOR_FRONT_PIN 0
 
+MagicServo left(LEFT_SERVO_PIN, NORMAL_FRONT_ORIENTATION); // Constructeur
+MagicServo right(RIGHT_SERVO_PIN, REVERSE_FRONT_ORIENTATION);// Constructeur
 
-MagicServo left(LEFT_SERVO_PIN, REVERSE_FRONT_ORIENTATION); // Constructeur
-MagicServo right(RIGHT_SERVO_PIN, NORMAL_FRONT_ORIENTATION);// Constructeur
+Sensor sensor_front(SENSOR_FRONT_PIN);
 
-Sensor sensor_1(0);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("Start");
 
-  sensor_1.setLimit(500);
+  sensor_front.setLimit(500);
 
   left.init();
   right.init();
+
+  left.front(100);
+  right.front(100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if (sensor_front.detect()) {
+    left.back(100);
+    delay(500);
+  }
+  else {
+    left.front(100);
+    right.front(100);
+  }
+  Serial.println(sensor_front.detect());
+  delay(100);
 }
