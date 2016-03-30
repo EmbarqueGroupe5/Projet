fbd080649ea231deebad9f514a0de867f6691cb0
Hraph <lepointgeek@gmail.com>
2016-03-30 22:19:41 +0200

    Test Magnetometer with Adafruit Library

---
+++ Tests/Test_Magnetometer_Adafruit/Test_Magnetometer_Adafruit.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Wire.h>
+
+
+//Magnetometer
+Adafruit_BNO055 magneto = Adafruit_BNO055(55);
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!magneto.begin()) {
+    Serial.println("No BNO !");
+    delay(1000);
+  }
+}
+
+void loop() {
+  //Magnetometer
+  sensors_event_t magneto_event;
+  magneto.getEvent(&magneto_event);
+  
+  Serial.println(magneto_event.magnetic.x);
+
+  delay(100);
+}
