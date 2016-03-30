ef19e65872c70e920d5d6ee47f4748bd34f40844
Hraph <lepointgeek@gmail.com>
2016-03-30 23:16:00 +0200

    Classe Magnetometer améliorée

--- Tests/Test_Magnetometer/Magnetometer.cpp
+++ Tests/Test_Magnetometer/Magnetometer.cpp
@@ -1,13 +1,55 @@
 #include "Arduino.h"
-#include "Vector.h"
 #include "Magnetometer.h"
+#include "utility/vector.h"
 
-Magnetometer::Magnetometer(int pin){
-  this->pin = pin;
+/*Magnetometer::Magnetometer(){
+  }*/
+
+bool Magnetometer::begin()
+{
+  bno055_opmode_t mode = OPERATION_MODE_NDOF;
+  /* Enable I2C */
+  Wire.begin();
+
+  /* Make sure we have the right device */
+  uint8_t id = read8(BNO055_CHIP_ID_ADDR);
+  if (id != BNO055_ID)
+  {
+    delay(1000); // hold on for boot
+    id = read8(BNO055_CHIP_ID_ADDR);
+    if (id != BNO055_ID) {
+      return false;  // still not? ok bail
+    }
+  }
+
+  /* Switch to config mode (just in case since this is the default) */
+  setMode(OPERATION_MODE_CONFIG);
+
+  /* Reset */
+  write8(BNO055_SYS_TRIGGER_ADDR, 0x20);
+  while (read8(BNO055_CHIP_ID_ADDR) != BNO055_ID)
+  {
+    delay(10);
+  }
+  delay(50);
+
+  /* Set to normal power mode */
+  write8(BNO055_PWR_MODE_ADDR, POWER_MODE_NORMAL);
+  delay(10);
+
+  write8(BNO055_PAGE_ID_ADDR, 0);
+
+  write8(BNO055_SYS_TRIGGER_ADDR, 0x0);
+  delay(10);
+  /* Set the requested operating mode */
+  setMode(mode);
+  delay(20);
+
+  return true;
 }
 
-std::vector<3> Magnetometer::getVector() {
-	std::vector<3> euler;
+imu::vector<3> Magnetometer::getVector() {
+  std::vector<3> euler;
   uint8_t buffer[6];
   memset (buffer, 0, 6);
 
@@ -15,22 +57,28 @@ std::vector<3> Magnetometer::getVector() {
   x = y = z = 0;
 
   /* Read vector data (6 bytes) */
-  readLen((adafruit_bno055_reg_t)vector_type, buffer, 6);
+  readLen((bno055_reg_t)VECTOR_EULER, buffer, 6); // Lit à l'adresse correspondante du EULER (enum registre)
 
   x = ((int16_t)buffer[0]) | (((int16_t)buffer[1]) << 8);
   y = ((int16_t)buffer[2]) | (((int16_t)buffer[3]) << 8);
   z = ((int16_t)buffer[4]) | (((int16_t)buffer[5]) << 8);
 
   /* Convert the value to an appropriate range and assign the value to the Vector type */
-  
+
   /* Convert to EULER (angle) */
   /* 1 degree = 16 LSB */
-  euler[0] = ((double)x)/16.0;
-  euler[1] = ((double)y)/16.0;
-  euler[2] = ((double)z)/16.0;
-      
-  
+  euler[0] = ((double)x) / 16.0;
+  euler[1] = ((double)y) / 16.0;
+  euler[2] = ((double)z) / 16.0;
+
   return euler;
+}
+
+void Magnetometer::setMode(bno055_opmode_t mode)
+{
+  _mode = mode;
+  write8(BNO055_OPR_MODE_ADDR, _mode);
+  delay(30);
 }
 
 bool Magnetometer::readLen(adafruit_bno055_reg_t reg, byte * buffer, uint8_t len) {
@@ -42,7 +90,7 @@ bool Magnetometer::readLen(adafruit_bno055_reg_t reg, byte * buffer, uint8_t len
 
   for (uint8_t i = 0; i < len; i++)
   {
-      buffer[i] = Wire.read();
+    buffer[i] = Wire.read();
   }
 
   return true;
@@ -50,10 +98,10 @@ bool Magnetometer::readLen(adafruit_bno055_reg_t reg, byte * buffer, uint8_t len
 
 bool Magnetometer::write8(adafruit_bno055_reg_t reg, byte value) {
   Wire.beginTransmission(_address);
-  
-Wire.write((uint8_t)reg);
-Wire.write((uint8_t)value);
-  
+
+  Wire.write((uint8_t)reg);
+  Wire.write((uint8_t)value);
+
   Wire.endTransmission();
   return true;
 }
--- Tests/Test_Magnetometer/Magnetometer.h
+++ Tests/Test_Magnetometer/Magnetometer.h
@@ -2,18 +2,82 @@
 #define Magnetometer_h
 
 #include "Arduino.h"
+#include "utility/vector.h"
 
-//Address enums
+#define BNO055_ADDRESS_B (0x29)
+#define BNO055_ID        (0xA0)
 
 class Magnetometer {
   public:
-    Magnetometer(int pin);
-    
+    //Address enums
+    typedef enum
+    {
+      /* PAGE0 REGISTER DEFINITION START*/
+      BNO055_CHIP_ID_ADDR                                     = 0x00,
+
+      /* Euler data registers */
+      BNO055_EULER_H_LSB_ADDR                                 = 0X1A,
+      BNO055_EULER_H_MSB_ADDR                                 = 0X1B,
+      BNO055_EULER_R_LSB_ADDR                                 = 0X1C,
+      BNO055_EULER_R_MSB_ADDR                                 = 0X1D,
+      BNO055_EULER_P_LSB_ADDR                                 = 0X1E,
+      BNO055_EULER_P_MSB_ADDR                                 = 0X1F,
+
+      /* Status registers */
+      BNO055_CALIB_STAT_ADDR                                  = 0X35,
+      BNO055_SELFTEST_RESULT_ADDR                             = 0X36,
+      BNO055_INTR_STAT_ADDR                                   = 0X37,
+
+      BNO055_SYS_CLK_STAT_ADDR                                = 0X38,
+      BNO055_SYS_STAT_ADDR                                    = 0X39,
+      BNO055_SYS_ERR_ADDR                                     = 0X3A,
+
+      /* Unit selection register */
+      BNO055_UNIT_SEL_ADDR                                    = 0X3B,
+      BNO055_DATA_SELECT_ADDR                                 = 0X3C
+    } bno055_reg_t;
+
+    typedef enum
+    {
+      POWER_MODE_NORMAL                                       = 0X00,
+      POWER_MODE_LOWPOWER                                     = 0X01,
+      POWER_MODE_SUSPEND                                      = 0X02
+    } bno055_powermode_t;
+
+    typedef enum
+    {
+      /* Operation mode settings*/
+      OPERATION_MODE_CONFIG                                   = 0X00,
+      OPERATION_MODE_ACCONLY                                  = 0X01,
+      OPERATION_MODE_MAGONLY                                  = 0X02,
+      OPERATION_MODE_GYRONLY                                  = 0X03,
+      OPERATION_MODE_ACCMAG                                   = 0X04,
+      OPERATION_MODE_ACCGYRO                                  = 0X05,
+      OPERATION_MODE_MAGGYRO                                  = 0X06,
+      OPERATION_MODE_AMG                                      = 0X07,
+      OPERATION_MODE_IMUPLUS                                  = 0X08,
+      OPERATION_MODE_COMPASS                                  = 0X09,
+      OPERATION_MODE_M4G                                      = 0X0A,
+      OPERATION_MODE_NDOF_FMC_OFF                             = 0X0B,
+      OPERATION_MODE_NDOF                                     = 0X0C
+    } bno055_opmode_t;
+
+
+    typedef enum
+    {
+      VECTOR_EULER         = BNO055_EULER_H_LSB_ADDR
+    } vector_type_t;
+
+    //Magnetometer();
+    bool  begin ();
+
 
   private:
-    bool  readLen ( adafruit_bno055_reg_t, byte* buffer, uint8_t len );
-    bool  write8  ( adafruit_bno055_reg_t, byte value );
-    std::vector<3> getVector();
+    bool  readLen (bno055_reg_t, byte* buffer, uint8_t len);
+    bool  write8  (bno055_reg_t, byte value);
+    void  setMode (bno055_opmode_t mode);
+    imu::vector<3> getVector();
+    uint8_t _address = BNO055_ADDRESS_B;
 };
 
 #endif
